a6907c14b2441ff384825c1ec1000494ce81836e
zain-Z <zai1999n@gmail.com>
2025-09-11 23:10:24 +0300

    Darts

---
+++ src/darts.ts
@@ -0,0 +1,24 @@
+export function score(x: number, y: number): number {
+const xSquared = x * x;
+const ySquared = y * y;
+
+
+const distance = Math.sqrt(xSquared + ySquared);
+
+if (distance <= 1) {
+    return 10; // Bullseye
+  } else if (distance <= 5) {
+    return 5; // Middle circle
+  } else if (distance <= 10) {
+    return 1; // Outer circle
+  } else {
+    return 0; // Miss
+  }
+}
+
+console.log(score(0, 0));       
+console.log(score(0.5, 0.5));   
+console.log(score(3, 4));       
+console.log(score(8, -6));      
+console.log(score(11, 0));      
+
